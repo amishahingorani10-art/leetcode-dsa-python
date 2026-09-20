74b49807479b23e483303aa202f8b135ea1bf7ca
Amisha <amishahingorani10@gmail.com>
2026-09-20 19:39:36 +0530

    Add Maximum Product Subarray Solution

---
+++ arrays/maximum_product_subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        maximum=nums[0]
+        minimum=nums[0]
+        answer=nums[0]
+        for i in range(1,len(nums)):
+            num=nums[i]
+            if num<0:
+                maximum,minimum=minimum,maximum
+            maximum=max(num,maximum*num)
+            minimum=min(num,minimum*num)
+
+            answer=max(answer,maximum)
+        return answer
+
+        
